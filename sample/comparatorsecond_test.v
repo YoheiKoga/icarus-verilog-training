049aa0e0bc833e1aac640f25c70cb756d2a22294
yoheikoga <prawv2@gmail.com>
2019-12-30 11:14:24 +0900

    fix comparatorsecond

--- sample/comparatorsecond_test.v
+++ sample/comparatorsecond_test.v
@@ -2,5 +2,46 @@
 module stimulus();
     // input
     reg x;
+    reg y;
+    // output
+    wire z;
+    // instantiate the unit under test (uut)
+    comparator uut (
+        .x(x),
+        .y(y),
+        .z(z)
+    );
+
+    integer period;
+
+    initial begin
+        x = 0;
+        y = 0;
+        period = 10;
+    end
+
+    initial begin
+        forever begin
+            #(period/3) x=~x;
+            #(period/3) x=~x;
+        end
+    end
+
+    initial begin
+        forever begin
+            #(period/2) y=~y;
+            #(period/2) y=~y;
+        end
+    end
+
+    initial begin
+        #100 $finish();
+    end
+
+    initial begin
+        $monitor("x=%d, y=%d, z=%d \n", x, y, z);
+        $dumpfile("comparatorsecond.vcd");
+        $dumpvars(0, stimulus);
+    end
 
 endmodule // stimulus
